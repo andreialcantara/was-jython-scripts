b820704d98eefa5c2e65e12b7d1063a9c417ea0c
andreialcantara <andreiluiz.alcantara@gmail.com>
2023-05-06 19:18:56 -0300

    committed  Changes to be committed:

---
+++ WebsphereJythonScripts/create-jdbcprovider.py
@@ -0,0 +1,39 @@
+import os
+
+# substitua pelo caminho real para o driver Oracle JDBC
+ORACLE_JDBC_DRIVER_PATH = '/path/to/oracle/jdbc/driver'
+
+# obtém o nome da Cell atual
+cell_name = AdminControl.getCell()
+print("Cell name: " + cell_name)
+
+# define uma lista de dicionários com as informações sobre os JDBC Providers
+jdbc_providers = [
+    {
+        'database_type': 'Oracle',
+        'provider_type': 'Oracle JDBC Driver',
+        'implementation_type': 'Connection pool data source',
+        'provider_name': 'Oracle JDBC Driver',
+        'provider_desc': 'Oracle JDBC Driver',
+        'classpath': '${ORACLE_JDBC_DRIVER_PATH}/ojdbc8.jar'
+    },
+    {
+        'database_type': 'Oracle',
+        'provider_type': 'Oracle JDBC Driver',
+        'implementation_type': 'XA data source',
+        'provider_name': 'Oracle JDBC Driver (XA)',
+        'provider_desc': 'Oracle JDBC Driver (XA)',
+        'classpath': '${ORACLE_JDBC_DRIVER_PATH}/ojdbc8.jar'
+    }
+]
+
+# percorre a lista de dicionários e cria um JDBC Provider para cada item
+for provider_info in jdbc_providers:
+    provider_id = '[-scope Cell=' + cell_name + ' -databaseType ' + provider_info['database_type'] + ' -providerType "' + provider_info['provider_type'] + '" -implementationType "' + provider_info['implementation_type'] + '" -name "' + provider_info['provider_name'] + '" -description "' + provider_info['provider_desc'] + '" -classpath [' + provider_info['classpath'] + '] -nativePath "" ]'
+    AdminTask.createJDBCProvider(provider_id)
+    print("JDBC Provider " + provider_info['provider_name'] + " created successfully.")
+
+# Salva as alterações na configuração da célula
+AdminConfig.save()
+
+print("All JDBC Providers created successfully.")
