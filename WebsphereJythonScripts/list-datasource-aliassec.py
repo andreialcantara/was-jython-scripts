74f0842568524c1ec67a367f4ceddad3a82d1fe4
Andrei Alcantara <61384012+andreialcantara@users.noreply.github.com>
2024-05-26 14:45:12 -0300

    Update list-datasource-aliassec.py
    adicionado script para gerar saida em formato csv

--- WebsphereJythonScripts/list-datasource-aliassec.py
+++ WebsphereJythonScripts/list-datasource-aliassec.py
@@ -22,3 +22,30 @@ for cell in cells:
 # Tratar a saída final após o loop
 output = re.sub(r'[^\x00-\x7F]', '', output)
 print(output)
+##################################################################################################################################
+# Lista todos os datasources configurados no nivel da celula do cluster websphere e exibe o alias de segurança que armazena as credencias de conexcão do datasource.
+# Gera a saida em formato csv
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#import re
+
+# Lista todos os datasources configurados no nivel da celula do cluster websphere e exibe o alias de segurança que armazena as credencias de conexcão do datasource.
+
+#cells = AdminConfig.list('Cell').split()
+#output = []
+
+#for cell in cells:
+#    cellName = AdminConfig.showAttribute(cell, 'name')
+#    datasources = AdminConfig.list('DataSource', cell).splitlines()
+#    for ds in datasources:
+#        dsName = AdminConfig.showAttribute(ds, 'name')
+#        securityAlias = AdminConfig.showAttribute(ds, 'authDataAlias')
+#        if securityAlias is None or securityAlias.strip() == "":
+#            securityAlias = "Nenhum alias de seguranca especificado"
+#        
+#        output.append([cellName, dsName, securityAlias])
+#
+# Imprimir os dados no formato CSV
+#print("Cell,Datasource,Alias de Seguranca")
+#for row in output:
+#    print(','.join(row))
